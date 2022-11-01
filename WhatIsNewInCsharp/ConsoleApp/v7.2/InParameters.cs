288c954db433f9146988ef6226fda67c5507c5ef
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-11-01 15:15:03 -0600

    Feat: Add examples about InParams, PrivateProtected and RefReadOnly

---
+++ WhatIsNewInCsharp/ConsoleApp/v7.2/InParameters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.v7._2
+{
+    struct Point
+    {
+        public double X, Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+               
+        }
+
+        #region V7.2 - RefReadOnlyVariables
+        private static v7._2.Point origin = new v7._2.Point();
+        public static ref readonly v7._2.Point Origin => ref origin;
+        #endregion
+
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+
+        public void Reset()
+        {
+            X = Y = 0;
+        }
+    }
+}
---
+++ WhatIsNewInCsharp/ConsoleApp/v7.2/PrivateProtected.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.v7._2
+{
+    public class Base
+    {
+        private int a;
+        protected internal int b;
+        // 1) Derived classes (ANY assembly)
+        // 2) Classes in same assembly
+
+        private protected int c;
+        // 1) Containing class
+        // 2) Derived classes but only in current assembly
+    }
+
+    public class Derived : Base
+    {
+        public Derived()
+        {
+            b = 1;
+            c = 2;
+        }
+    }
+}
