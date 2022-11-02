63c0d6d14828e03c31d0dc5b0bd2a04fc33c2f36
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-11-02 17:06:11 -0600

    Feat: Add examples about NullableReferencesTypes, IndicesAndRanges, DefaultInterfaceMembers and PatternMatching in C#8

--- WhatIsNewInCsharp/ConsoleApp/v7/PatternMatching.cs
+++ WhatIsNewInCsharp/ConsoleApp/v7/PatternMatching.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp.v7._2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,37 @@ namespace ConsoleApp.v7
 {
     public class Shape
     {
-
+        public (int, int) Coordinate;
     }
 
     public class Rectangle : Shape
     {
         public int Width, Height;
+
+        public Rectangle()
+        {
+
+        }
+        public void Deconstruct(out (int, int) p, out int w, out int h)
+        {
+            p = Coordinate;
+            w = Width;
+            h = Height;
+        }
     }
 
     public class Circle : Shape
     {
+        public int Radius;
 
+        public Circle()
+        {
+                
+        }
+        public void Deconstruct(out (int, int) p, out int r)
+        {
+            p = Coordinate;
+            r = Radius;
+        }
     }
 }
